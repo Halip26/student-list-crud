6ce3be8e487a39962b8c01a17eabe8f7b34144d4
Halip26 <halipuddin.angko@gmail.com>
2024-07-28 10:48:14 +0800

    change the config value

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ app.secret_key = "don't bullshit a bullshitter"
 
 app.config["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_USER"] = "root"
-# app.config["MYSQL_PASSWORD"] = "halip-brightchamps26"
-app.config["MYSQL_DB"] = "crud"
+app.config["MYSQL_PASSWORD"] = ""
+app.config["MYSQL_DB"] = "crud_flask"
 
 mysql = MySQL(app)
 
